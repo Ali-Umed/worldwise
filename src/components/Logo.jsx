3922d452c13ff659017faef1ae872417a48d7b6f
Ali-Umed <aliumed844@gmail.com>
2024-05-30 22:58:47 +0300

    change style text

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 
 function Logo() {
-  return <Link to="/">KurdWanderer</Link>;
+  return (
+    <Link to="/">
+      {" "}
+      <h2> Kurd-Wanderer</h2>
+    </Link>
+  );
 }
 
 export default Logo;
