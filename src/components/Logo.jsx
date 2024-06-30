e812a8b3bf6e8c091a7cd99e3ba763da10286cd8
Ali-Umed <aliumed844@gmail.com>
2024-07-01 01:02:31 +0300

    fix: add position and open box correctly

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Logo() {
+  return (
+    <Link to="/">
+      {" "}
+      <h2> Kurd-Wanderer</h2>
+    </Link>
+  );
+}
+
+export default Logo;
